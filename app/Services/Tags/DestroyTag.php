34e23a2b5eb4039354ca09db75e68861daa5eb4c
Nick Klein <1294247+nickklein@users.noreply.github.com>
2020-01-05 15:50:14 -0800

    -Remove vendor files - refacor NotesController and TagsController

---
+++ app/Services/Tags/DestroyTag.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace notes\Services\Tags;
+
+use notes\Models\Tags;
+use notes\Models\TagsRel;
+
+class DestroyTag
+{
+
+    /**
+     * Destroys a single tag from specific user
+     *
+     * @return collection
+     */
+    public function single(int $userId, array $fields)
+    {
+        if (!empty($fields['tag_name'])) {
+            $tag = Tags::checkTags($fields['tag_name'])->first();
+            if (!empty($tag)) {
+                $tagsRelationships = TagsRel::where([
+                    'tag_id' => $tag->tag_id,
+                    'user_id' => $userId,
+                    'note_id' => $fields['page_id'],
+                ]);
+                // Get listings in order to clean them up later
+                $fetchTagsRelationships = $tagsRelationships->get();
+                if ($tagsRelationships->delete()) {
+                    // Clean out unused tags, and return tags
+                    $this->cleanLastTags($fetchTagsRelationships);
+                    return $tagsRelationships->first();
+                }
+            }
+        }
+    }
+
+    /**
+     * Destroys a all tags from a specific note and user
+     *
+     */
+    public function multiple(int $userId, int $note_id)
+    {
+        // Delete tags associated to a note_id and user_id
+        $tagsRelationships = TagsRel::where([
+            'note_id' => $note_id,
+            'user_id' => $userId,
+        ]);
+        $fetchTagsRelationships = $tagsRelationships->get();
+        $tagsRelationships->delete();
+
+        // Clean out old tags
+        $this->cleanLastTags($fetchTagsRelationships);
+    }
+
+    /**
+     * Cleans out unused tags inside the Tag table.
+     *
+     */
+    private function cleanLastTags($tagsRelationships): void
+    {
+        // Check if tags exist inside TagsRel table, if not delete
+        $tagIds = $tagsRelationships->pluck('tag_id')->toArray();
+        foreach($tagIds as $id) {
+            // If it doesn't exist then delete tag from tags table
+            $count = TagsRel::where('tag_id', $id)->count();
+            if (!$count) {
+                Tags::where('tag_id', $id)->delete();
+            }
+        }
+
+    }
+}
