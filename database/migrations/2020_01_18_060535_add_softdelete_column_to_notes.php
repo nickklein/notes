9b77b666e62e01c88126d7b2d26147aad8fa4fd1
Nick Klein <1294247+nickklein@users.noreply.github.com>
2020-01-18 11:04:30 -0800

    added soft delete for notes (#41)
    * added soft delete for notes
    
    * remove unnecessary route

---
+++ database/migrations/2020_01_18_060535_add_softdelete_column_to_notes.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSoftdeleteColumnToNotes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notes', function (Blueprint $table) {
+            //
+            $table->softDeletes();
+        });
+    }
+
+}
